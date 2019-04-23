72f4374a18cc23a675842b4af13cecc069e1fc47
aditi <ank55@cornell.edu>
2019-04-23 08:36:23 -0400

    differentialte files

--- phys3310/clean500pF.py
+++ phys3310/clean500pF.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 
 _FILENAME = '500pF-bindingport.csv'
 _INPUT = []
-_MAX_LINE = 170
+_MAX_LINE = 200
 
 _F_COL=0
 _X_COL=6
@@ -47,43 +47,52 @@ def analyze(start, end):
     perr = np.sqrt(np.diag(pcov))
     return params, perr
 
-read_file()
+# read_file()
 
-x_data=extract_col(_INPUT, _F_COL)
-y_data=extract_col(_INPUT, _X_COL)
+# x_data=extract_col(_INPUT, _F_COL)
+# y_data=extract_col(_INPUT, _X_COL)
 
-params, pcov = optimize.curve_fit(test_func, 
-    x_data,
-    y_data,
-    p0=[500e-12, 90e-9])
-    #p0=None)
+# params, pcov = optimize.curve_fit(test_func, 
+#     x_data,
+#     y_data,
+#     p0=[500e-12, 90e-9])
+#     #p0=None)
 
-plt.figure(figsize=(6, 4))
-plt.ylabel(r'$|X_S|$')
-plt.xlabel('Frequency')
-plt.title('500pF capacitor fit')
-plt.scatter(x_data, y_data, label='Data')
-plt.plot(x_data, test_func(x_data, params[0], params[1]),
-         label='Fitted function', color='red')
+# plt.figure(figsize=(6, 4))
+# plt.scatter(x_data, y_data, label='Data')
+# plt.plot(x_data, test_func(x_data, params[0], params[1]),
+#          label='Fitted function')
+# plt.plot(x_data, test_func(x_data, params[0], params[1]),
+#          label='Fitted function')
 
-plt.legend(loc='best')
+# plt.legend(loc='best')
 
-plt.savefig('500pFfit.png')
-
-# c=[]
-# cerr=[]
-# x=[]
-# y=[]
-# for start in range(2,50):
-#     for end in range(170, 171):
-#         params, err=analyze(start, end)
-#         print(start)
-#         print(params[0])
-#         x+=[start]
-#         y+=[params[0]]
-
-# plt.plot(x,y)
 # plt.show()
-print(params)
-perr = np.sqrt(np.diag(pcov))
-print(perr)
+
+c=[]
+cerr=[]
+x=[]
+y=[]
+y2=[]
+for start in range(30,31):
+    for end in range(170, 220):
+        params, err=analyze(start, end)
+        print(start)
+        print(end)
+        print(params[0])
+        print(err[0])
+        x+=[end]
+        y+=[params[0]]
+        y2+=[err[1]]
+
+plt.plot(x,y,label="C")
+plt.plot(x,y2,label=r"$\sigma$")
+plt.ylabel('value of best fit C')
+plt.xlabel('starting frequency point number')
+plt.title('Data cleaning: Start frequency')
+plt.legend()
+# plt.show()
+plt.savefig('500pFstartFrequency.png')
+# print(params)
+# perr = np.sqrt(np.diag(pcov))
+# print(perr)
